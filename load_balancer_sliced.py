fed59d81a92af5d48ab56316328f7d228512e5fb
Tapan <tapankarnik@gmail.com>
2020-12-16 16:06:41 +0000

    Updated tests with dummy code app_sliced

---
+++ load_balancer_sliced.py
@@ -0,0 +1,62 @@
+from flask import Flask, request
+import json, yaml, os
+import pika
+import docker
+
+load_balancer = Flask(__name__)
+
+i = 0
+
+@load_balancer.route('/first_hello', methods=['POST'])
+def router():
+    return b"OK"
+
+@load_balancer.route('/DCN', methods=['POST'])
+def loadbalancer():
+    if request.is_json:
+        job = request.json
+
+        try:
+            if len(job['job_name'])>20 or len(job['job_name'])==0 or job['job_name'] == None:
+                raise Exception('Invalid Job Name')
+            if job['num_jobs']<0 or job['num_jobs']==0 or job['num_jobs']>100:
+                raise Exception('Invalid Number of Jobs')
+            if job['job_id']<0 or job['job_id']>=job['num_jobs']:
+                raise Exception('Invalid Job ID')
+            if job['job_duration']<=0 or job['job_duration']>30:
+                raise Exception('Invalid Job Duration')
+
+        except Exception:
+            return "Invalid Input", 400
+
+        global i
+
+        
+        #client = docker.from_env()
+        #containers = client.containers.list()
+        #CONTAINER_NAME = [container.name for container in containers if 'rabbitmq' in container.name]
+        #connection = pika.BlockingConnection(
+        #    pika.ConnectionParameters(host=CONTAINER_NAME[i]) # Round-Robin Algorithm
+        #    )
+        #i = (i+1)%len(CONTAINER_NAME)
+
+        #channel = connection.channel()
+
+        #channel.queue_declare(queue='queue')
+
+        #channel.basic_publish(
+        #        exchange='',
+        #        routing_key='queue',
+        #        body=json.dumps(job)
+        #        )
+
+        #print(" [x] Sent Job ID ", job['job_id'], " under Job name ", job['job_name'])
+
+        #connection.close()
+        return b'OK\n'
+    else:
+        return b'Input not JSON', 400
+
+if __name__=="__main__":
+    load_balancer.run(host='0.0.0.0')
+
--- test_load_balancer.py
+++ test_load_balancer.py
@@ -1,5 +1,5 @@
-from load_balancer import load_balancer
-
+from load_balancer_sliced import load_balancer
+import json
 import pytest
 
 @pytest.fixture
@@ -20,15 +20,113 @@ def test_hello(client):
 def test_loadbalancer1(client):
     payload = dict(
         job_name = "Tapan",
-        job_id = 2,
-        num_jobs = 12,
-        job_duration = 2,
+        job_id = 4,
+        num_jobs = 11,
+        job_duration = 3,
         )
-
-    result = client.post('/DCN', json=payload)
+    payload = json.dumps(payload)
+    result = client.post('/DCN', data=payload, content_type='application/json')
     assert b"OK" in result.data
 
 def test_loadbalancer2(client):
+    payload = dict(
+        job_name = None,
+        job_id = 1,
+        num_jobs = 2,
+        job_duration = 2,
+        )
+    payload = json.dumps(payload)
+    result = client.post('/DCN', data=payload, content_type='application/json')
+    assert b"Invalid Input" in result.data
+
+def test_loadbalancer3(client):
+    payload = dict(
+        job_name = "",
+        job_id = 1,
+        num_jobs = 2,
+        job_duration = 2,
+        )
+    payload = json.dumps(payload)
+    result = client.post('/DCN', data=payload, content_type='application/json')
+    assert b"Invalid Input" in result.data
+
+def test_loadbalancer4(client):
+    payload = dict(
+        job_name = "Tapan",
+        job_id = 1,
+        num_jobs = 0,
+        job_duration = 15,
+        )
+    payload = json.dumps(payload)
+    result = client.post('/DCN', data=payload, content_type='application/json')
+    assert b"Invalid Input" in result.data
+
+def test_loadbalancer5(client):
+    payload = dict(
+        job_name = "j-1234858585858585858588585948359348594385983",
+        job_id = 1,
+        num_jobs = 50,
+        job_duration = 2,
+        )
+    payload = json.dumps(payload)
+    result = client.post('/DCN', data=payload, content_type='application/json')
+    assert b"Invalid Input" in result.data
+def test_loadbalancer6(client):
+    payload = dict(
+        job_name = "Tapan",
+        job_id = 1,
+        num_jobs = 101,
+        job_duration = 29,
+        )
+    payload = json.dumps(payload)
+    result = client.post('/DCN', data=payload, content_type='application/json')
+    assert b"Invalid Input" in result.data
+
+def test_loadbalancer7(client):
+    payload = dict(
+        job_name = "Tapan",
+        job_id = 1,
+        num_jobs = 99,
+        job_duration = 0,
+        )
+    payload = json.dumps(payload)
+    result = client.post('/DCN', data=payload, content_type='application/json')
+    assert b"Invalid Input" in result.data
+
+def test_loadbalancer8(client):
+    payload = dict(
+        job_name = "Tapan",
+        job_id = 1,
+        num_jobs = 100,
+        job_duration = 31,
+        )
+    payload = json.dumps(payload)
+    result = client.post('/DCN', data=payload, content_type='application/json')
+    assert b"Invalid Input" in result.data
+
+def test_loadbalancer9(client):
+    payload = dict(
+        job_name = "Tapan",
+        job_id = -1,
+        num_jobs = 100,
+        job_duration = 31,
+        )
+    payload = json.dumps(payload)
+    result = client.post('/DCN', data=payload, content_type='application/json')
+    assert b"Invalid Input" in result.data
+
+def test_loadbalancer10(client):
+    payload = dict(
+        job_name = "Tapan",
+        job_id = 102,
+        num_jobs = 100,
+        job_duration = 31,
+        )
+    payload = json.dumps(payload)
+    result = client.post('/DCN', data=payload, content_type='application/json')
+    assert b"Invalid Input" in result.data
+
+def test_loadbalancer11(client):
     payload = dict(
         job_name = "",
         job_id = -1,
@@ -38,6 +136,3 @@ def test_loadbalancer2(client):
 
     result = client.post('/DCN', json=payload)
     assert b"Invalid Input" in result.data
-
-
-
